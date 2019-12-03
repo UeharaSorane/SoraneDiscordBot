93721ae13011d9ccad193df514b027df35b5c021
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2019-12-03 19:08:21 +0800

    Update Dice.js

--- roll/Dice.js
+++ roll/Dice.js
@@ -1,6 +1,14 @@
+var rply = ["",""];
+
+function RandomNumber(Max){
+	return Math.floor((Math.random() * Max) + 1);
+}
+
 function xdy(many,x,y,content){
+	
 }
 
 module.exports = {
+	RandomNumber,
 	xdy
 }
