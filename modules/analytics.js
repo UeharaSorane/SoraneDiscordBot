a03b6abfc5b491f675e2233d1feec133df34d6a0
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2019-12-03 18:20:48 +0800

    Update analytics.js

--- modules/analytics.js
+++ modules/analytics.js
@@ -4,3 +4,16 @@ require('fs').readdirSync('./roll/').forEach(function(file) {
 	  exports[name] = require('../roll/' + file);
 	}
 });
+
+function parseInput(msg){
+	
+	let msgSplitor = (/\S+/ig);	
+	let mainMsg = msg.content.match(msgSplitor); //定義輸入字串
+	let trigger = mainMsg[0].toString().toLowerCase(); //指定啟動詞在第一個詞&把大階強制轉成細階
+	
+	if(trigger == "/星爆$") return ["rply","c8763"];
+	
+	else{
+		console.log(msg.author.username + "說了:「" + msg.content + "」");
+	}
+}
