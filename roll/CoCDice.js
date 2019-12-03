e0b0721dd5a907e2fbb416d28d8969dcb47da768
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2019-12-03 22:51:37 +0800

    Update CoCDice.js

--- roll/CoCDice.js
+++ roll/CoCDice.js
@@ -1,6 +1,12 @@
+var rply = ["",""];
 const Dice = require('./Dice.js');
 
 function ccb(PassRate,content,Bonus){
+	if(isNaN(PassRate)||isNaN(Bonus)){
+		rply[0] = "rply";
+		rply[1] = "ccb格式錯誤!正確用法為[ccb 成功值 描述 加成值]";
+		return rply;
+	}
 }
 
 module.exports = {
