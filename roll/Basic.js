a93752aed01edcf0bfab6115644c3d4faccac536
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2019-12-03 18:51:55 +0800

    Update Basic.js

--- roll/Basic.js
+++ roll/Basic.js
@@ -1,1 +1,15 @@
+var rply = ["",""];
 
+function TellTime(){
+	rply[0]= "rply";
+	
+	var Time = new Date();
+	rply[1] = "現在是:西元" + Time.getFullYear() + "/" + (Time.getMonth() + 1) + "/" + Time.getDate();
+	
+	return rply;
+	
+}
+
+module.exports{
+	TellTime
+};
