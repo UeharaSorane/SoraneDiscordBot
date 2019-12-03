6a2ca577d684c80abda519aa8abf8673d0b365b9
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2019-12-03 16:19:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const Express = require('express');
+
+var app = express();
+
+app.post('/', bot.parser());
+
+//因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
+var server = app.listen(process.env.PORT || 8080, function() {
+	var port = server.address().port;
+	console.log("App now running on port", port);
+	console.log("好，沒事");
+});
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
