3f1215a2e8370c6f41c1eb342d37d66298250f10
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2019-12-03 18:33:46 +0800

    Update analytics.js

--- modules/analytics.js
+++ modules/analytics.js
@@ -11,7 +11,7 @@ function parseInput(msg){
 	let mainMsg = msg.content.match(msgSplitor); //定義輸入字串
 	let trigger = mainMsg[0].toString().toLowerCase(); //指定啟動詞在第一個詞&把大階強制轉成細階
 	
-	if(trigger == "/星爆$") return ["rply","c8763"];
+	if(trigger.match(/^星爆$/) != null) return ["rply","c8763"];
 	
 	else{
 		console.log(msg.author.username + "說了:「" + msg.content + "」");
