7c548b7e3a883bedd9472a758bca06ab84d2ad06
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2019-12-03 15:58:58 +0800

    Update index.js

--- index.js
+++ index.js
@@ -9,5 +9,4 @@ client.on('message', msg => {
     msg.reply(msg);
 });
 
-client.login('NjUxMzEzNDk1NDg0MTM3NDcz.XeYUCw.4Z2V2bBjhRkFtzFvxANs0vzIxq8').
-  then(console.log("登入成功！")).catch(console.error);
+client.login('NjUxMzEzNDk1NDg0MTM3NDcz.XeYUCw.4Z2V2bBjhRkFtzFvxANs0vzIxq8').catch(console.error);
