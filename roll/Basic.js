8a795999f05d1047caeb2eea2680ad97ec9f5d7b
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2019-12-14 02:00:22 +0800

    Update Basic.js

--- roll/Basic.js
+++ roll/Basic.js
@@ -4,7 +4,8 @@ function TellTime(){
 	rply[0]= "rply";
 	
 	var Time = new Date();
-	rply[1] = "現在是:西元" + Time.getFullYear() + "/" + (Time.getMonth() + 1) + "/" + Time.getDate();
+	rply[1] = "現在是:西元" + Time.getFullYear() + "/" + (Time.getMonth() + 1) + "/" + Time.getDate() + "\
+				\n" + Time.getHours() + ":" + Time.getMinutes() + ":" + Time.getSeconds();
 	
 	return rply;
 	
