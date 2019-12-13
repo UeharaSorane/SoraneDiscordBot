feb3cdc7dc768c0461f2136d7786bba1b9e8fddf
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2019-12-14 02:37:45 +0800

    Update DBmanage.js

--- roll/DBmanage.js
+++ roll/DBmanage.js
@@ -1,1 +1,11 @@
 var mongoose = require('mongoose');
+var mongoDB = 'mongodb://b88009005:b09050905@ds229312.mlab.com:29312/linetest';
+
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+const Schema = mongoose.Schema;
+
+mongoose.connect(mongoDB, function (err) {
+	if(err) throw err;
+	else console.log('連線成功，可以正常吃芒果!');
+});
