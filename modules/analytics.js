71f41a006bd756266528d90c77430f306ca26fbe
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2020-01-14 21:43:22 +0800

    Update analytics.js

--- modules/analytics.js
+++ modules/analytics.js
@@ -5,6 +5,8 @@ require('fs').readdirSync('./roll/').forEach(function(file) {
 	}
 });
 
+var Chatmode = false;
+
 function parseInput(Sys,msg,uid,uname){
 	
 	let msgSplitor = (/\S+/ig);	
@@ -12,32 +14,47 @@ function parseInput(Sys,msg,uid,uname){
 	console.log(mainMsg);
 	let trigger = mainMsg[0].toString().toLowerCase(); //指定啟動詞在第一個詞&把大階強制轉成細階
 	
+	if(Chatmode == true){
+		if(trigger.match(/^聊天模式$/) != null){
+			Chatmode = false;
+			return ["rply","聊天模式開啟，指令將可以作用"];
+		}
+		
+		return ["NaC",""];
+	}else{
+	
 /////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////分析開始//////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////
-	
-	if(trigger.match(/^報時$/) != null) return exports.Basic.TellTime();
-	else if(trigger.match(/運氣/) != null) return exports.funny.randomLuck(mainMsg);
-	else if(trigger.match(/^ccb$/) != null){
-		return exports.CoCDice.ccb(mainMsg[1],mainMsg[2],mainMsg[3],Sys,uname);
-	}
-	else if(trigger.match(/^ccrt$/) != null) return exports.CoCCrazy.ccrt(Sys,uname);
-	else if(trigger.match(/^ccsu$/) != null) return exports.CoCCrazy.ccsu(Sys,uname);
-	
-	//////////基本擲骰//////////
-	else if(msg.match(/\d/) != null){
-		return exports.Dice.NormalDy(mainMsg);
-	}
-	//////////CoC擲骰//////////
-	
-	///////全部都不是的狀況/////
-	else{
-		return ["NaC"," "];
-	}
-	//////////////////////////
+		if(trigger.match(/^聊天模式$/) != null){
+			if(!Chatmode){
+				Chatmode = true;
+				return ["rply","聊天模式開啟，指令將無法作用"];
+			}
+		}
+		else if(trigger.match(/^報時$/) != null) return exports.Basic.TellTime();
+		else if(trigger.match(/運氣/) != null) return exports.funny.randomLuck(mainMsg);
+		else if(trigger.match(/^ccb$/) != null){
+			return exports.CoCDice.ccb(mainMsg[1],mainMsg[2],mainMsg[3],Sys,uname);
+		}
+		else if(trigger.match(/^ccrt$/) != null) return exports.CoCCrazy.ccrt(Sys,uname);
+		else if(trigger.match(/^ccsu$/) != null) return exports.CoCCrazy.ccsu(Sys,uname);
+
+		//////////基本擲骰//////////
+		else if(msg.match(/\d/) != null){
+			return exports.Dice.NormalDy(mainMsg);
+		}
+		//////////CoC擲骰//////////
+
+		///////全部都不是的狀況/////
+		else{
+			return ["NaC"," "];
+		}
+		//////////////////////////
 /////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////分析結束//////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////
+	)
 }
 
 module.exports = {
