f2f477d71b1df025d0f706349028c75239636124
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2020-01-14 22:04:42 +0800

    Update Talking.js

--- roll/Talking.js
+++ roll/Talking.js
@@ -1,1 +1,16 @@
+var dice = require('./Dice.js');
+var {Wit, log} = require("node-wit");
 
+var WitClient = new Wit({
+  	accessToken: "QEF3CPJXJGTFFS3OJUXEOPXU5V5UJGIX" // 同上
+});
+
+var Chat;
+
+function Chatting(InputChat){
+	Chat = InputChat;
+}
+
+module.exports = {
+	Chatting
+};
