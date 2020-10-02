234b9729d84376f6dac6a5046b2df118ec2e1331
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2020-10-03 07:35:11 +0800

    Update LineTalking.js

--- roll/LineTalking.js
+++ roll/LineTalking.js
@@ -1,1 +1,21 @@
+const DBmanage = require('./DBmanage.js');
 
+var TalkChannal = DBmanage.getChannalDB();
+
+function ChannalCheck(UserID,UserN){
+	for(var a = 0; a<talkChannal.length;a++){
+		if(UserID == talkChannal[a].channal_line_id){
+			return 0;
+		}
+	}
+	var TCL = talkChannal.length;
+	talkChannal[TCL] = {
+		"channal_id" : TCL,
+		"channal_line_id" : UserID,
+		"channal_name" : UserN
+	};
+	DBmanage.ChannalUpdate(talkChannal[TCL]);
+	
+	
+	
+}
