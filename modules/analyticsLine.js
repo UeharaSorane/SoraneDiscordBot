8112bc6bfc6db8b08b7afc506ff7a3644177bb36
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2020-10-03 07:24:07 +0800

    Update analyticsLine.js

--- modules/analyticsLine.js
+++ modules/analyticsLine.js
@@ -16,7 +16,8 @@ function parseInput(msg,uid,uname){
 /////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////分析開始//////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////
-	if(trigger.match(/^報時$/) != null) return exports.Basic.TellTime();
+	if(trigger.match(/^私訊$/) != null)return exports.LineTalking.SecretTalk(uid,uname,mainMsg[1]);
+	else if(trigger.match(/^報時$/) != null) return exports.Basic.TellTime();
 	else if(trigger.match(/運氣/) != null) return exports.funny.randomLuck(mainMsg);
 	else if(trigger.match(/^ccb$/) != null){
 		return exports.CoCDice.ccb(mainMsg[1],mainMsg[2],mainMsg[3],uname);
