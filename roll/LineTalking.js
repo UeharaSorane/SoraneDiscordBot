cca6a1d957d6ed997caaf17d239e157782eff180
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2020-10-03 07:37:11 +0800

    Update LineTalking.js

--- roll/LineTalking.js
+++ roll/LineTalking.js
@@ -15,7 +15,36 @@ function ChannalCheck(UserID,UserN){
 		"channal_name" : UserN
 	};
 	DBmanage.ChannalUpdate(talkChannal[TCL]);
-	
-	
-	
 }
+function SecretTalk(UserID,Channal,myText) {
+	///匿名淦話
+	if(Channal == null){
+		rply[0] = 'rply';
+		rply[1] = '這是目前有登錄的頻道清單：';
+		for(var a = 0;a< talkChannal.length;a++){
+			rply[1] += '\n'+(a+1) +'\.'+talkChannal[a].channal_name;
+		}
+		rply[1] += '\n\n 想要進行匿名對話，請輸入[淦話 頻道編號 對話內容]';
+		        return rply;
+	}else if(talkChannal[Channal-1] == null){
+		rply[0] = 'rply';
+		rply[1] = '找不到該頻道喔';
+		return rply;
+		
+	}else{
+		if(myText == null){
+			rply[0] = 'rply';
+		        rply[1] = '頻道名稱:'+ talkChannal[Channal-1].channal_name+'\
+                                \n\n 想要進行匿名對話，請輸入[淦話 頻道編號 對話內容]';
+		        return rply;
+		}else{
+			rply[0] = 'push';
+		        bot.push(talkChannal[Channal-1].channal_line_id,myText);
+		        return rply;
+		}
+	}
+}
+
+module.exports = {
+	SecretTalk
+};
