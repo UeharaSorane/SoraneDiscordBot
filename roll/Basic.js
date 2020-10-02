6f1170ecc97066728af46f17adbb02a84db2caa9
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2020-10-03 06:32:11 +0800

    Update Basic.js

--- roll/Basic.js
+++ roll/Basic.js
@@ -3,7 +3,7 @@ var rply = ["",""];
 
 function TellTime(){
 	
-	var Time = new Date();
+	var Time = new Date().toLocaleString('zh-TW', {timeZone: 'Asia/Taipei'});
 	
 	rply[0]= "rply";
 	rply[1] = "現在是:西元" + Time.getFullYear()  + "/" + (Time.getMonth() + 1) + "/" + Time.getDate() + "\
