9e0943a80d9d59de1256db95ecca857e4877a9cb
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2020-10-04 02:16:08 +0800

    Update analyticsLine.js

--- modules/analyticsLine.js
+++ modules/analyticsLine.js
@@ -17,26 +17,20 @@ function parseInput(msg,uid,uname){
 ////////////////////////////////////分析開始//////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////
 	
-	//////////基本功能//////////
-	if(trigger.match(/^私訊$/) != null)return exports.LineTalking.SecretTalk(uid,mainMsg[1],mainMsg[2]);
-	else if(trigger.match(/^目錄$/) != null) return exports.LineMenu.menu(uname);
-	
-	//////////趣味功能//////////
+	if(trigger.match(/^報時$/) != null) return exports.Basic.TellTime();
 	else if(trigger.match(/運氣/) != null) return exports.funny.randomLuck(mainMsg);
-	
+	else if(trigger.match(/^ccb$/) != null){
+		return exports.CoCDice.ccb(mainMsg[1],mainMsg[2],mainMsg[3],uname);
+	}
+	else if(trigger.match(/^ccrt$/) != null) return exports.CoCCrazy.ccrt(uname);
+	else if(trigger.match(/^ccsu$/) != null) return exports.CoCCrazy.ccsu(uname);
 
 	//////////基本擲骰//////////
 	else if(trigger.match("d") !=null || mainMsg[1].match("d") !=null){
 		return exports.Dice.NormalDy(mainMsg);
 	}
-	
 	//////////CoC擲骰//////////
-	else if(trigger.match(/^ccrt$/) != null) return exports.CoCCrazy.ccrt(uname);
-	else if(trigger.match(/^ccsu$/) != null) return exports.CoCCrazy.ccsu(uname);
-	/*else if(trigger.match(/^ccb$/) != null){
-		return exports.CoCDice.ccb(mainMsg[1],mainMsg[2],mainMsg[3],uname);
-	}*/
-	
+
 	///////全部都不是的狀況/////
 	else{
 		return ["NaC"," "];
