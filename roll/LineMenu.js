ec16e1db9799febd3d940d72a1b47c3c36257d3a
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2020-10-03 09:14:49 +0800

    Update LineMenu.js

--- roll/LineMenu.js
+++ roll/LineMenu.js
@@ -2,7 +2,35 @@ var rply = [];
 
 function menu(UserN){
 	rply[0] = 'rply';	
-	rply[1] = {
+	rply[1] ={
+		  "type": "template",
+		  "altText": "很抱歉，你的Line版本不支援此系統，建議輸入help查看可使用的指令",
+		  "template": {
+		  	"type": "buttons",
+		  		"text":"嗨！" + UserN + "，請問要做什麼？",
+		  		"actions": [{
+					"type": "message",
+					"label": "開啟CoC選單",
+					"text": "CoC選單"},
+					{
+					"type": "message",
+					"label": "開啟娛樂選單",
+					"text": "娛樂選單"
+					},
+		      			{
+					"type": "message",
+					"label": "打開幫助",
+					"text": "help"
+					},
+					{
+					"type": "message",
+					"label": "閒聊",
+					"text": "嘿空音"
+					}
+				]
+		}
+	}
+		/*{
 		"type" : "template",
 		"altText": "很抱歉，你的Line版本不支援此系統，建議輸入help查看可使用的指令",
 		"template" : {
@@ -11,10 +39,12 @@ function menu(UserN){
 				{
 					"title": "嗨！" + UserN + "，請問要做什麼？",
 					"text": "TRPG骰子選單:",
-					"actions": [{
+					"actions": [
+						{
 						"type": "message",
 						"label": "基本骰子功能",
-						"text": "基本骰子功能"},
+						"text": "基本骰子功能"
+						},
 						{
 						"type": "message",
 						"label": "CoC骰子功能",
@@ -25,10 +55,12 @@ function menu(UserN){
 				{
 					"title": "嗨！" + UserN + "，請問要做什麼？",
 					"text": "娛樂選單:",
-					"actions": [{
+					"actions": [
+						{
 						"type": "message",
 						"label": "今日運氣",
-						"text": "今天的運氣"},
+						"text": "今天的運氣"
+						},
 						{
 						"type": "message",
 						"label": "立死旗",
