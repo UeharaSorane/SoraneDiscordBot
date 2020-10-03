304f9aa9ace4ee579757b8258d4eaee094c29c3d
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2020-10-03 09:20:25 +0800

    Update LineMenu.js

--- roll/LineMenu.js
+++ roll/LineMenu.js
@@ -3,34 +3,6 @@ var rply = [];
 function menu(UserN){
 	rply[0] = 'rply';	
 	rply[1] ={
-		  "type": "template",
-		  "altText": "很抱歉，你的Line版本不支援此系統，建議輸入help查看可使用的指令",
-		  "template": {
-		  	"type": "buttons",
-		  		"text":"嗨！" + UserN + "，請問要做什麼？",
-		  		"actions": [{
-					"type": "message",
-					"label": "開啟CoC選單",
-					"text": "CoC選單"},
-					{
-					"type": "message",
-					"label": "開啟娛樂選單",
-					"text": "娛樂選單"
-					},
-		      			{
-					"type": "message",
-					"label": "打開幫助",
-					"text": "help"
-					},
-					{
-					"type": "message",
-					"label": "閒聊",
-					"text": "嘿空音"
-					}
-				]
-		}
-	}
-		/*{
 		"type" : "template",
 		"altText": "很抱歉，你的Line版本不支援此系統，建議輸入help查看可使用的指令",
 		"template" : {
@@ -51,45 +23,10 @@ function menu(UserN){
 						"text": "CoC骰子功能"
 						}
 					]
-				},
-				{
-					"title": "嗨！" + UserN + "，請問要做什麼？",
-					"text": "娛樂選單:",
-					"actions": [
-						{
-						"type": "message",
-						"label": "今日運氣",
-						"text": "今天的運氣"
-						},
-						{
-						"type": "message",
-						"label": "立死旗",
-						"text": "插旗"
-						}
-					]
-				},
-				{
-					"title": "嗨！" + UserN + "，請問要做什麼？",
-					"text": "其他選單:",
-					"actions": [{
-						"type": "message",
-						"label": "打開文字版說明書",
-						"text": "help"},
-						{
-						"type": "message",
-						"label": "報時",
-						"text": "報時"},
-						{
-						"type": "message",
-						"label": "閒聊",
-						"text": "空音"
-						}
-					]
 				}
-				
 			]
 		}
-	}*/
+	}
 
 	return rply;
 }
