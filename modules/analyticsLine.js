1e6e633b3ab9fbb64a1a843ace050d681961aa46
施彥任 <bernie135792468@gmail.com>
2021-06-23 15:53:09 +0800

    OK

--- modules/analyticsLine.js
+++ modules/analyticsLine.js
@@ -20,18 +20,18 @@ function parseInput(msg,uid,uname){
 	if(trigger.match(/^報時$/) != null) return exports.Basic.TellTime();
 	else if(trigger.match(/^私訊$/) != null) return exports.LineTalking.SecretTalk(uid,mainMsg[1],mainMsg[2]);
 	else if(trigger.match(/運氣/) != null) return exports.funny.randomLuck(mainMsg);
-	else if(trigger.match(/^ccb$/) != null){
-		return exports.CoCDice.ccb(mainMsg[1],mainMsg[2],mainMsg[3],uname);
-	}
 	else if(trigger.match(/^ccrt$/) != null) return exports.CoCCrazy.ccrt(uname);
 	else if(trigger.match(/^ccsu$/) != null) return exports.CoCCrazy.ccsu(uname);
+	else if(trigger.match(/^目錄$/) != null) return exports.LineMenu.menu(uname);
 
 	//////////基本擲骰//////////
 	else if(trigger.match("d") !=null || trigger.match(/^[0-9]+$/) !=null){
 		return exports.Dice.NormalDy(mainMsg);
 	}
 	//////////CoC擲骰//////////
-
+	else if(trigger.match(/^ccb$/) != null){
+		return exports.CoCDice.ccb(mainMsg[1],mainMsg[2],mainMsg[3],uname);
+	}
 	///////全部都不是的狀況/////
 	else{
 		return ["NaC"," "];
